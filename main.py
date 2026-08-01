56d4c418f982a24a60d2e76e57e70cff4218e85a
Whoose-name <mhmd0husnain@gmail.com>
2026-08-02 00:29:35 +0530

    feat: implement custom eye landmark visualization using MediaPipe

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ face_mesh = mp_face.FaceMesh(
     min_tracking_confidence=0.5
 )
 cap = cv2.VideoCapture(0)
+RIGHT_EYE = [33, 133, 159, 145, 153, 154, 155]
+LEFT_EYE = [362, 263, 386, 374, 380, 381, 382]
+
+
 while True:
     success, frame = cap.read()
     if not success:
@@ -16,13 +20,32 @@ while True:
     rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     results = face_mesh.process(rgb_frame)
     if results.multi_face_landmarks:
+        ''' #proto type 1
         for face_landmarks in results.multi_face_landmarks:
             mp_draw.draw_landmarks(
                 frame,
                 face_landmarks,
                 mp_face.FACEMESH_TESSELATION
+            )'''
+        face = results.multi_face_landmarks[0]
+        h, w, _ = frame.shape
+        for idx in RIGHT_EYE + LEFT_EYE:
+            x = int(face.landmark[idx].x * w)
+            y = int(face.landmark[idx].y * h)
+            cv2.circle(frame, (x, y), 2, (0, 255, 0), -1)
+            cv2.putText(
+                frame,
+                str(idx),
+                (x + 5, y - 5),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.4,
+                (0, 255, 255),
+                1
             )
-    cv2.imshow("Face Mesh", frame)
+
+
+        
+    cv2.imshow("Eye Landmarks", frame)
     if cv2.waitKey(1) & 0xFF == 27:
         break
 cap.release()
